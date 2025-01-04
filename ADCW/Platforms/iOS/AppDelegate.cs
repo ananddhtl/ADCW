d34266ac5a0ce8d1f64e2589ab11731e985e7ba4
Anand <anddhital@gmail.com>
2025-01-05 00:59:51 +0545

    Add project files.

---
+++ ADCW/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ADCW
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
